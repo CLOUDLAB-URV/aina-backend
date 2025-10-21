50b2bcf55e95d16e2869a8b700c7e67eed2e807e
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2025-10-21 08:32:23 +0200

    Remove unused model

--- api/schemas/index.py
+++ api/schemas/index.py
@@ -9,12 +9,3 @@ class IndexCreate(BaseModel):
 
 class IndexInfo(IndexCreate):
     id: int = Field(description="The unique identifier of the index")
-
-
-class IndexUpdate(BaseModel):
-    id: int = Field(description="The unique identifier of the index")
-    name: str | None = Field(default=None, description="The name of the index")
-    config: dict | None = Field(
-        default=None, description="The configuration of the index"
-    )
-    index_type: str | None = Field(default=None, description="The type of the index")
